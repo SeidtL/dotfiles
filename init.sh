6ecdab4b051a1cd352da2d98d8a25956718fa7fa
SeidtL <SeidtL@163.com>
2024-06-12 17:12:12 +0800

    UPDATE: config of nvim

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 CFG_ROOT="$HOME/.config"
 mkdir -p $CFG_ROOT/nvim 
-ln -s $CFG_ROOT/nvimrc $CFG_ROOT/nvim/init.lua
+
+ln -s $CFG_ROOT/nvim.lua $CFG_ROOT/nvim/init.lua
 ln -s $CFG_ROOT/zshrc $HOME/.zshrc 
 ln -s $CFG_ROOT/tmux.conf $HOME/.tmux.conf
