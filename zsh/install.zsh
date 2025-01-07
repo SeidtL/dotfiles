52d1774521a2f1d3a98d1bbd6a14bf96d3623402
SeidtL <SeidtL@163.com>
2025-01-07 11:03:37 +0800

    update zsh

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -1,30 +1,29 @@
 #!/usr/bin/env zsh 
 set -e
-
+ZSH_PREFIX=$HOME/.local/share/zsh
 function download() {
-    curl -f -o $2 $1
+    curl -f -o $ZSH_PREFIX/$2 $1
 }
 function download_omz() {
     local url="https://gitee.com/mirrors/oh-my-zsh/raw/master/$1"
     download $url $1
 }
 
-rm -rf $HOME/.config/zsh/completion/
-rm -rf $HOME/.config/zsh/lib/
-rm -rf $HOME/.config/zsh/theme/
 
-mkdir -p lib 
-mkdir -p completion 
-mkdir -p theme 
+mkdir -p $ZSH_PREFIX/lib/
+mkdir -p $ZSH_PREFIX/site-functions/
+mkdir -p $ZSH_PREFIX/theme/
 
 download_omz lib/clipboard.zsh
 download_omz lib/history.zsh 
 download_omz lib/key-bindings.zsh 
 download_omz lib/theme-and-appearance.zsh 
 download_omz lib/git.zsh
-download https://raw.gitcode.com/gh_mirrors/co/conda-zsh-completion/raw/main/_conda completion/_conda
+download https://raw.gitcode.com/gh_mirrors/co/conda-zsh-completion/raw/main/_conda site-functions/_conda
 
-git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git $HOME/.config/zsh/theme/p10k
+rm -rf $ZSH_PREFIX/theme/p10k
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git $ZSH_PREFIX/theme/p10k
 
 ln -sf $HOME/.config/zsh/zshrc $HOME/.zshrc 
 ln -sf $HOME/.config/zsh/zshenv $HOME/.zshenv
+
