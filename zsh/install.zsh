5eb446136af91168a39f5e24f33d5b7b503140d0
SeidtL <SeidtL@163.com>
2025-01-07 11:21:58 +0800

    update rust

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh 
 set -e
+
 ZSH_PREFIX=$HOME/.local/share/zsh
 function download() {
     curl -f -o $ZSH_PREFIX/$2 $1
@@ -9,20 +10,26 @@ function download_omz() {
     download $url $1
 }
 
-
+# lib 
 mkdir -p $ZSH_PREFIX/lib/
-mkdir -p $ZSH_PREFIX/site-functions/
-mkdir -p $ZSH_PREFIX/theme/
-
 download_omz lib/clipboard.zsh
 download_omz lib/history.zsh 
 download_omz lib/key-bindings.zsh 
 download_omz lib/theme-and-appearance.zsh 
 download_omz lib/git.zsh
-download https://raw.gitcode.com/gh_mirrors/co/conda-zsh-completion/raw/main/_conda site-functions/_conda
 
+# theme 
+mkdir -p $ZSH_PREFIX/theme/
 rm -rf $ZSH_PREFIX/theme/p10k
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git $ZSH_PREFIX/theme/p10k
+
+# site-functions
+mkdir -p $ZSH_PREFIX/site-functions/
+if command -v rustup &>/dev/null; then 
+    cp "$(rustc +${${(z)$(rustup default)}[1]} --print sysroot)"/share/zsh/site-functions/_cargo $ZSH_PREFIX/site-functions/_cargo
+    rustup completions zsh > $ZSH_PREFIX/site-functions/_rustup
+fi
+download https://raw.gitcode.com/gh_mirrors/co/conda-zsh-completion/raw/main/_conda site-functions/_conda
 
 ln -sf $HOME/.config/zsh/zshrc $HOME/.zshrc 
 ln -sf $HOME/.config/zsh/zshenv $HOME/.zshenv
