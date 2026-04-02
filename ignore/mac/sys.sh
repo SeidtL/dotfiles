de7befe42d225bcc274fa37a40b53b828a93e38b
SeidtL <SeidtL@163.com>
2026-04-02 14:41:00 +0800

    update: mac

--- ignore/mac/sys.sh
+++ ignore/mac/sys.sh
@@ -1,6 +1,6 @@
 defaults write -g ApplePressAndHoldEnabled -bool false
-defaults write -g InitialKeyRepeat -int 15
-defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 2
 
 defaults write -g NSAutomaticCapitalizationEnabled -bool false
 
