4962e304132408958abccb37e0bce3ddd703e51b
SeidtL <SeidtL@163.com>
2026-05-24 23:00:59 +0800

    update: nvim theme

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "moon", -- 保持 moon 风格
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = { italic = false },
+        variables = { italic = false },
+        strings = { italic = false },
+      },
+    },
+  },
+}
